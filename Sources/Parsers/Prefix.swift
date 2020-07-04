70794689d8a253ed5b6348870de8b2ffefd13384
Rhys Morgan <rhysm@me.com>
2020-07-04 13:39:28 +0100

    Added prefix(while:) parser and relevant tests

---
+++ Sources/Parsers/Prefix.swift
@@ -0,0 +1,16 @@
+//
+//  Prefix.swift
+//  Parsers
+//
+//  Created by Rhys Morgan on 04/07/2020.
+//
+
+public extension Parser where A == Substring {
+	static func prefix(while predicate: @escaping (Character) -> Bool) -> Parser {
+		Parser { str in
+			let prefix = str.prefix(while: predicate)
+			str.removeFirst(prefix.count)
+			return prefix
+		}
+	}
+}
