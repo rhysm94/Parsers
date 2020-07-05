38b2082a2725a4c95f2414d96274664ba2a78981
Rhys Morgan <rhysm@me.com>
2020-07-05 23:11:13 +0100

    Added more tests to demonstrate more complex zeroOrMore uses

--- Tests/ParsersTests/CombinatorsTests/ZeroOrMoreTests.swift
+++ Tests/ParsersTests/CombinatorsTests/ZeroOrMoreTests.swift
@@ -10,6 +10,10 @@ import XCTest
 
 final class ZeroOrMoreTests: XCTestCase {
 	let zeroOrMoreSpaces = zeroOrMore(.literal(" "))
+	let commaSeparatedInts = zeroOrMore(.int, separatedBy: .literal(","))
+	let commaOrNewLineSeparatedInts = zeroOrMore(.int, separatedBy: Parser.char.flatMap {
+		$0 == "," || $0 == "\n" ? .always(()) : .never
+	})
 
 	private func testHelloWorld(prefixedSpaces: Int = 0) throws {
 		let input = String(repeating: " ", count: prefixedSpaces) + "Hello World"
@@ -30,5 +34,60 @@ final class ZeroOrMoreTests: XCTestCase {
 
 	func testZeroOrMoreSpaces_Many() throws {
 		try testHelloWorld(prefixedSpaces: 30)
+	}
+
+	func testZeroOrMore_CSV_0Values_NoTrailing() throws {
+		let input = "abcde"
+
+		let result = commaSeparatedInts(input)
+		let match = try XCTUnwrap(result.match)
+
+		XCTAssertTrue(match.isEmpty)
+		XCTAssertEqual("abcde", result.rest)
+	}
+
+	func testZeroOrMore_CSV_4Values_NoTrailing() throws {
+		let input = "1,2,3,4 Hello World"
+
+		let result = commaSeparatedInts(input)
+		let match = try XCTUnwrap(result.match)
+
+		XCTAssertEqual([1,2,3,4], match)
+		XCTAssertEqual(" Hello World", result.rest)
+	}
+
+	func testZeroOrMore_CSV_4Values_Trailing() throws {
+		let input = "1,2,3,4,"
+
+		let result = commaSeparatedInts(input)
+		let match = try XCTUnwrap(result.match)
+
+		XCTAssertEqual([1,2,3,4], match)
+		XCTAssertEqual(",", result.rest)
+	}
+
+	func testZeroOrMore_CSV_10Values_NoTrailing_NoRest() throws {
+		let input = "1,2,3,4,5,6,7,8,9,0"
+
+		let result = commaSeparatedInts(input)
+		let match = try XCTUnwrap(result.match)
+
+		XCTAssertEqual([1,2,3,4,5,6,7,8,9,0], match)
+		XCTAssertTrue(result.rest.isEmpty)
+	}
+
+	func testLongZeroOrMoreCSV() throws {
+		let longInput = """
+		1,2,3,4,5
+		6,7,8,9,10
+		11,12,13,14,15
+		16,17,18,19,20
+		"""
+
+		let result = commaOrNewLineSeparatedInts(longInput)
+		let match = try XCTUnwrap(result.match)
+
+		XCTAssertEqual(Array(1...20), match)
+		XCTAssertTrue(result.rest.isEmpty)
 	}
 }
