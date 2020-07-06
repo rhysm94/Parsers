fcd74e4bd5adee64d1ca3c86048cf0b00e0d7c6c
Rhys Morgan <rhysm@me.com>
2020-07-07 00:20:07 +0100

    Added the ZeroOrOne Parser

---
+++ Tests/ParsersTests/CombinatorsTests/ZeroOrOneTests.swift
@@ -0,0 +1,31 @@
+//
+//  ZeroOrOneTests.swift
+//  ParsersTests
+//
+//  Created by Rhys Morgan on 07/07/2020.
+//
+
+import Parsers
+import XCTest
+
+final class ZeroOrOneTests: XCTestCase {
+	private let hyphenParser = zeroOrOne(.literal("-"))
+
+	func testZeroCase() {
+		let result = hyphenParser.run("125")
+		XCTAssertNotNil(result.match)
+		XCTAssertEqual("125", result.rest)
+	}
+
+	func testOneCase() {
+		let result = hyphenParser.run("-125")
+		XCTAssertNotNil(result.match)
+		XCTAssertEqual("125", result.rest)
+	}
+
+	func testMoreCase() {
+		let result = hyphenParser.run("---abc")
+		XCTAssertNil(result.match)
+		XCTAssertEqual("---abc", result.rest)
+	}
+}
