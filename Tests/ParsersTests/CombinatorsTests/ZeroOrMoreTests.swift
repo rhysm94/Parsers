be6b0e19b32ad7ce94d92bbc181e71f08ad4af33
Rhys Morgan <rhysm@me.com>
2020-07-10 08:28:20 +0100

    Oops, hadn't fixed ZeroOrMoreTests' allTests

--- Tests/ParsersTests/CombinatorsTests/ZeroOrMoreTests.swift
+++ Tests/ParsersTests/CombinatorsTests/ZeroOrMoreTests.swift
@@ -92,13 +92,13 @@ final class ZeroOrMoreTests: XCTestCase {
 	}
 
 	static let allTests = [
-		(testZeroOrMoreSpaces_Zero, "testZeroOrMoreSpaces_Zero"),
-		(testZeroOrMoreSpaces_One, "testZeroOrMoreSpaces_One"),
-		(testZeroOrMoreSpaces_Many, "testZeroOrMoreSpaces_Many"),
-		(testZeroOrMore_CSV_0Values_NoTrailing, "testZeroOrMore_CSV_0Values_NoTrailing"),
-		(testZeroOrMore_CSV_4Values_NoTrailing, "testZeroOrMore_CSV_4Values_NoTrailing"),
-		(testZeroOrMore_CSV_4Values_Trailing, "testZeroOrMore_CSV_4Values_Trailing"),
-		(testZeroOrMore_CSV_10Values_NoTrailing_NoRest, "testZeroOrMore_CSV_10Values_NoTrailing_NoRest"),
-		(testLongZeroOrMoreCSV, "testLongZeroOrMoreCSV")
+		("testZeroOrMoreSpaces_Zero", testZeroOrMoreSpaces_Zero),
+		("testZeroOrMoreSpaces_One", testZeroOrMoreSpaces_One),
+		("testZeroOrMoreSpaces_Many", testZeroOrMoreSpaces_Many),
+		("testZeroOrMore_CSV_0Values_NoTrailing", testZeroOrMore_CSV_0Values_NoTrailing),
+		("testZeroOrMore_CSV_4Values_NoTrailing", testZeroOrMore_CSV_4Values_NoTrailing),
+		("testZeroOrMore_CSV_4Values_Trailing", testZeroOrMore_CSV_4Values_Trailing),
+		("testZeroOrMore_CSV_10Values_NoTrailing_NoRest", testZeroOrMore_CSV_10Values_NoTrailing_NoRest),
+		("testLongZeroOrMoreCSV", testLongZeroOrMoreCSV)
 	]
 }
